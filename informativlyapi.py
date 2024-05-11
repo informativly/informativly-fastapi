953f3605f9d7396332b3d4cbeb4bed9eb7365063
informativly <informativly+github@gmail.com>
2024-05-12 01:10:14 +0200

    update application

---
+++ informativlyapi.py
@@ -0,0 +1,10 @@
+# Import FastAPI
+from fastapi import FastAPI
+
+# Create an instance of the FastAPI class
+app = FastAPI()
+
+# Define a route for the root endpoint
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
